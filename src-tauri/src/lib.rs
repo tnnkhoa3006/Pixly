54ca5be896a1611dfec1ae86f11fa0766ed092ea
Khoa Tran <khoat0360@gmail.com>
2026-05-08 09:41:50 +0700

    build ios

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,28 +1,32 @@
-use tauri::{AppHandle, Manager};
+use tauri::AppHandle;
+
+#[cfg(not(any(target_os = "android", target_os = "ios")))]
+use tauri::Manager;
 
 #[tauri::command]
-fn complete_splash(app: AppHandle) {
-    if let Some(main_window) = app.get_webview_window("main") {
-        let _ = main_window.show();
-        let _ = main_window.set_focus();
-    }
+fn complete_splash(_app: AppHandle) {
+    #[cfg(not(any(target_os = "android", target_os = "ios")))]
+    {
+        if let Some(main_window) = _app.get_webview_window("main") {
+            let _ = main_window.show();
+            let _ = main_window.set_focus();
+        }
 
-    if let Some(splash_window) = app.get_webview_window("splash") {
-        let _ = splash_window.close();
+        if let Some(splash_window) = _app.get_webview_window("splash") {
+            let _ = splash_window.close();
+        }
     }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut builder = tauri::Builder::default()
+    let builder = tauri::Builder::default()
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init());
 
     #[cfg(not(any(target_os = "android", target_os = "ios")))]
-    {
-        builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
-    }
+    let builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
 
     builder
         .invoke_handler(tauri::generate_handler![complete_splash])
